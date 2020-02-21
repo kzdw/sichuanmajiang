02f2638265b0b073b4ea3aea03a103dc5af765b3
kzdw <449241216@qq.com>
2020-02-21 12:49:08 +0800

    增加边张中张注释

--- src/main/java/fan/Fan.java
+++ src/main/java/fan/Fan.java
@@ -11,7 +11,7 @@ import java.util.List;
  * @author Henry Zhou
  */
 public interface Fan {
-    //门清 幺九将对 大对子 杠 一条龙 夹心五 卡二条 清一色 板板高 金钩钓
+    //门清中张 幺九将对 大对子 杠 一条龙 夹心五 卡二条 清一色 板板高 金钩钓 边张
 
     /**
      * 根据配置计算番
